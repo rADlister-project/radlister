901ae8fb80a5f5e78da4a8bb65ba2dec737059d6
MrOffer <thomas.e.offer@gmail.com>
2020-01-17 10:45:14 -0600

    seeder and migration table fixed

--- migration.sql
+++ migration.sql
@@ -15,8 +15,20 @@ CREATE TABLE ads (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     user_id INT UNSIGNED NOT NULL,
     title VARCHAR(240) NOT NULL,
+    price float,
     description TEXT NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (user_id) REFERENCES users(id)
-        ON DELETE CASCADE
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
+
+ CREATE TABLE category (
+     id INT,
+     description Varchar(240)
+);
+
+ Create Table ad_category (
+     ad_id Int NOT NULL,
+     cat_id Int NOT NULL,
+     Foreign Key (ad_id) REFERENCES ads(id) ON DELETE CASCADE ,
+     foreign key (cat_id) references category(id) ON DELETE CASCADE
+ );
---
+++ seeder.sql
@@ -0,0 +1,8 @@
+INSERT INTO category (id, description)
+VALUES (1, 'For Sale'),
+       (2, 'Free Stuff'),
+       (3, 'Help Wanted'),
+       (4, 'Gigs'),
+       (5, 'Services'),
+       (6, 'Housing'),
+       (7, 'Community');
